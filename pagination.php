76e290585e1a0c081a277210cf0920e31107e06b
komputerkerja <komputerkerja100@gmail.com>
2020-05-25 12:15:13 +0700

    menghapus file dan menambahkan script baru di dalam pagination.php

--- pagination.php
+++ pagination.php
@@ -1,9 +1,21 @@
 <?php 
 
 $conn = mysqli_connect('localhost','root','','mugen');
-$query = 'SELECT * FROM general_ledger LIMIT 0 , 20';
-$result = mysqli_query($conn,$query);
+$query = "SELECT * FROM general_ledger";
 
-echo mysqli_num_rows($result);
+// Lihat berapa banyak data yang ada
+$totalData = mysqli_num_rows(mysqli_query($conn,$query));
+// tentukan berapa banyak data yang akan di tampilkan
+$dataPerHalaman = 10;
+// jumlah halaman = totalData / dataPerhalaman (ceil() pembulatan ke atas)
+$jumlahHalaman = ceil($totalData / $dataPerHalaman);
+// halaman yang aktif ambil dari url
+$halamanAktif = isset($_GET['halaman']) ? $_GET['halaman'] : 1;
+// Awal Data yang akan di tampilkan 
+$awalData = ($dataPerHalaman*$halamanAktif)-$dataPerHalaman;
 
+
+
+$result = mysqli_query($conn,$query . " LIMIT $awalData , $dataPerHalaman");
+var_dump($result);
 ?>
